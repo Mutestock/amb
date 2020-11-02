a926983ff563e35ea578e06aa8022631b5708a44
Mutestock <doublenegative@protonmail.com>
2020-11-02 14:47:54 +0100

    Dockerfile rust. Main.rs docker compatibility

--- amb_back/src/main.rs
+++ amb_back/src/main.rs
@@ -1,12 +1,17 @@
-use warp::Filter;
+use warp::{Filter, Rejection, Reply};
+
+type Result<T> = std::result::Result<T, Rejection>;
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+    let health_route = warp::path!("health").and_then(health_handler);
 
-    warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+    let routes = health_route.with(warp::cors().allow_any_origin());
+
+    println!("Started server at localhost:8000");
+    warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
+}
+
+async fn health_handler() -> Result<impl Reply> {
+    Ok("OK")
 }
